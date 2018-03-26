7606e5b386ba016f814e11d9f0857f94dcf96ccb
Andreas Lundgren <andreas.lundgren@coesia.com>
2018-03-26 15:27:02 +0200

    more things

--- lock.sh
+++ lock.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Take a screenshot:
-scrot /tmp/screen.png
+image_file=/tmp/screen.png
 
-# Create a blur on the shot:
-convert /tmp/screen.png -paint 1 -swirl 360 /tmp/screen.png
-
-# Add the lock to the blurred image:
-[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
-
-# Pause music (mocp and mpd):
-mocp -P
-mpc pause
+# Screenshot and blur
+resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
+filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor' 
+ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 -vf "$filters" "$image_file"
 
 # Lock it up!
-i3lock -e -f -c 000000 -i /tmp/screen.png
+i3lock -e -f -c 000000 -i $image_file
 
 # If still locked after 20 seconds, turn off screen.
 sleep 20 && pgrep i3lock && xset dpms force off
